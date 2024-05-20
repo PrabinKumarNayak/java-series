e39c88b0e52f96c27e44bdd1ed5fcbfee62a935f
PrabinKumarNayak <nayakdilipkumar818@gmail.com>
2024-05-20 23:06:03 +0530

    program 1 to 6

---
+++ day-1/prog2.js
@@ -0,0 +1,3 @@
+console.log("PC23IST-040");
+console.log("Prabin Kumar Nayak");
+console.log("Drawing");
---
+++ day-1/prog5.js
@@ -0,0 +1,6 @@
+a=20;
+b=70;
+s=a+b;
+console.log("First number =",a);
+console.log("Second number =",b);
+console.log("Sum=",s);
