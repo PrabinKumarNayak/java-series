562c876c963bddeb0a2b25103def50319f763d8a
PrabinKumarNayak <nayakdilipkumar818@gmail.com>
2024-09-04 19:53:27 +0530

    datatypes

---
+++ 02_datatype.js
@@ -0,0 +1,25 @@
+"use strict";//treat all JS code as newer version
+
+// alert(3+3); we are using nodejs , not browser
+
+//code readabiltiy should be there
+
+let name="prabin"
+let age=18
+let isLoggedIn =false
+let state= null
+
+//PRIMITIVE DATATYPE
+//number=>2 to power 53
+//bigint
+//string=> ""
+//boolean=> true/false
+//null=> standalone value
+//undefined
+//symbol=> unique
+
+
+//OBJECT
+
+console.log(typeof null);
+console.log(typeof undefined);
