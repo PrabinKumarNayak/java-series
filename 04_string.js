6e19bc43d0091f9f50531fd735335d96a962fe45
PrabinKumarNayak <nayakdilipkumar818@gmail.com>
2024-09-08 19:58:29 +0530

    string

---
+++ 04_string.js
@@ -0,0 +1,27 @@
+let value= 3
+let negValue= -value
+console.log(negValue);
+
+let str1="Hello"
+let str2=" prabin"
+
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+"2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+
+console.log(+true);//increment in boolean value "true" , output=>1
+//console.log(true+); //Error
+
+console.log(+"");//0
+
+let num1, num2, num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
