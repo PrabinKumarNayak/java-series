96a59b9e3a576275e82791f39031ceafba07f568
PrabinKumarNayak <nayakdilipkumar818@gmail.com>
2024-10-10 19:03:37 +0530

    json

---
+++ 15_json.js
@@ -0,0 +1,22 @@
+const course = {
+    coursename:"js in hindi",
+    Price:"506",
+    courseInstructor:"prabin"
+}
+
+const{courseInstructor: instructor}=course
+
+console.log(courseInstructor);//prabin
+console.log(instructor);//prabin
+//API
+// {
+//     name:"prabin",
+//     coursename: "js in hindi",
+//     price:"50000"
+// }//json, keys are generally string
+
+[
+    {},
+    {},
+    {}
+]//api as array
