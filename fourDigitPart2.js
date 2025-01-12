9c3a2f1b2b7afd56425f10bec81c39ee790ea0c3
PrabinKumarNayak <nayakdilipkumar818@gmail.com>
2025-01-12 18:18:23 +0530

    problem

---
+++ fourDigitPart2.js
@@ -0,0 +1,20 @@
+let n=5678;
+let fo=n%10;
+let t1=parseInt(n/10);
+let th= t1%10;
+let t2=parseInt(t1/10);
+let se=t2%10;
+let fi=parseInt(t2/10);
+let sum=fi+th;
+let mul=se*fo;
+console.log("Number=",n);
+console.log("Fourth Digit=",fo);
+console.log("Third Digit=",th);
+console.log("Second Digit=",se);
+console.log("First Digit=",fi);
+console.log("sum of first and third digit is: ",sum);
+console.log("multiplication of second and fourth digit is: ",mul);
+
+
+
+
