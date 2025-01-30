3dc30919e112b4fe35e6fec389ceea5bcea48901
PrabinKumarNayak <nayakdilipkumar818@gmail.com>
2025-01-30 20:39:51 +0530

    sumEvenOddFourDigit

---
+++ sumEvenOddFourDigit.js
@@ -0,0 +1,16 @@
+let n=5678;
+let fo=n%10;
+let t1=parseInt(n/10);
+let th= t1%10;
+let t2=parseInt(t1/10);
+let se=t2%10;
+let fi=parseInt(t2/10);
+console.log("Number=",n);
+console.log("Fourth Digit=",fo);
+console.log("Third Digit=",th);
+console.log("Second Digit=",se);
+console.log("First Digit=",fi);
+esum=se+fo;
+osum=fi+th;
+console.log(esum,"is the sum of even places");
+console.log(osum,"is the sum of odd places");
