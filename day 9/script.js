b680dd9da5e4d9f68d6c1178853b94e6f7ec94b2
PrabinKumarNayak <nayakdilipkumar818@gmail.com>
2025-02-03 12:39:43 +0530

    day 9

---
+++ day 9/script.js
@@ -0,0 +1,9 @@
+const text = document.getElementById("text")
+const header = document.getElementById("header")
+const add = document.getElementById("add")
+
+let  value = ""
+text.addEventListener("input",()=>{
+    value = text.value;
+})
+
